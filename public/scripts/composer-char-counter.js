c6cf82d49c1cc8b80c231c1338a9e6c3b3cf45cc
sergey <sbarchshevskiy@gmail.com>
2021-02-24 00:21:08 +0000

    minor work

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,26 @@
+const $ = require("jquery");
+
+
+// $("#input").on("input", function() {
+//   $("#count").text(this.value.length);
+// });
+
+// $(document).ready(function() {
+//   // --- our code goes here ---
+// });
+
+
+
+// $(document).ready(function() {
+//   $('.new-tweet').on('input', function() {
+//     const counter = $(".counter").text(this.value.length);
+
+    
+//   });
+
+// }); // document ready
+
+
+// $(document).ready(function() {
+//   console.log();
+// });
