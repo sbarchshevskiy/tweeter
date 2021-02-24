b06c0a1afa973c156a061b84b6e405cb98f19f6a
sergey <sbarchshevskiy@gmail.com>
2021-02-24 19:52:44 +0000

    makes css better

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,25 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+
+const createTweetElement = function() {
+
+
+};
+
+const tweetData = {
+  "user": {
+    "name": "Newton",
+    "avatars": "https://i.imgur.com/73hZDYK.png",
+    "handle": "@SirIsaac"
+  },
+  "content": {
+    "text": "If I have seen further it is by standing on the shoulders of giants"
+  },
+  "created_at": 1461116232227
+};
+
+const $tweet = createTweetElement(tweetData);
+
+console.log($tweet); 
+$('#tweets-container').append($tweet);
